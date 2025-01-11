ddf6b0af6cecd693c3e53ae39aad65f69a643e4e
moataz kamal <moatazkmal@gmail.com>
2025-01-12 00:44:48 +0200

    making ui dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { FaReact } from "react-icons/fa";
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TempAndDetails from './components/TempAndDetails';
 import Forecast from './components/Forecast';
-import getFOrmattedWeatherData from './services/weatherService';
+import getFormattedWeatherData from './services/weatherService';
+
 function App() {
 
+  const [query, setQuery] = useState({ q: 'london' });
+  const [units, setUnits] = useState('metric');
+  const [weather, setWeather] = useState(null);
 
   const getWeather = async () => {
-    const data = await getFOrmattedWeatherData({q: 'berlin'})
-    console.log(data);
+    await getFormattedWeatherData({ ...query, units }).then(data => {
+      setWeather(data);
+    });
+    console.log(weather);
   };
 
-
-  getWeather();
-
+  useEffect(() => {
+    getWeather();
+  }, [query, units]);
 
   return (
-    <div className='mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700'> 
+    <div className='mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700'>
       <TopButtons />
       <Inputs />
-      <TimeAndLocation />
-      <TempAndDetails />
-      <Forecast />
-      <Forecast />
+
+      {weather && (
+        <>
+          <TimeAndLocation  weather={weather}/>
+          <TempAndDetails weather={weather}/>
+          <Forecast />
+          <Forecast />
+        </>
+      )}
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TempAndDetails.jsx
+++ src/components/TempAndDetails.jsx
@@ -4,26 +4,28 @@ import { FiWind } from "react-icons/fi"
 import { GiSunrise, GiSunset } from "react-icons/gi"
 import { MdKeyboardArrowDown, MdKeyboardArrowUp, MdKeyboardDoubleArrowUp } from "react-icons/md"
 
-const TempAndDetails = () => {
+const TempAndDetails = ({weather: {
+  details, icon, temp, temp_min, temp_max, sunrise, sunset, speed, humidity, feels_like,
+}}) => {
 
   const verticalDetails = [
     {
       id: 1,
       Icon: FaThermometerEmpty,
       title: "Real Feel",
-      value: "22°"
+      value: `${feels_like.toFixed()}°`
     },
     {
       id: 2,
       Icon: BiSolidDropletHalf,
       title: "Humidity",
-      value: "346%"
+      value: `${humidity.toFixed()}%`
     },
     {
       id: 3,
       Icon: FiWind,
       title: "Wind",
-      value: "11 Km/h"
+      value: `${speed.toFixed()}Km/h`
     },
   ]
 
@@ -33,25 +35,25 @@ const TempAndDetails = () => {
       id: 1,
       Icon: GiSunrise,
       title: "Sunrise",
-      value: "05:33 AM"
+      value: sunrise,
     },
     {
       id: 2,
       Icon: GiSunset,
       title: "SUnset",
-      value: "08:33 PM"
+      value: sunset,
     },
     {
       id: 3,
       Icon: MdKeyboardArrowUp,
       title: "High",
-      value: "37°"
+      value: `${temp_max.toFixed()}°`
     },
     {
       id: 4,
       Icon: MdKeyboardArrowDown,
       title: "Low",
-      value: "7°"
+      value: `${temp_min.toFixed()}°`
     },
   ]
 
@@ -61,16 +63,16 @@ const TempAndDetails = () => {
   return (
     <div>
       <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
-        <p>Rain</p>
+        <p>{details}</p>
       </div>
 
       <div className="flex flex-row items-center justify-between py-3">
         <img
-          src="https://openweathermap.org/img/wn/01d@2x.png"
+          src={icon}
           alt="weather icon"
           className="w-20"
         />
-        <p className="text-5xl">34°</p>
+        <p className="text-5xl">{`${temp.toFixed()}°`}</p>
 
         <div className="flex flex-col space-y-3 items-start">
           {verticalDetails.map(({ id, Icon, title, value }) => (
